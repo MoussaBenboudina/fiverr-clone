4d92c54b822e37609a803dda566a639d8e20bc61
unknown <benboudinamousa@gmail.com>
2026-03-24 16:01:02 -0700

    Update API config and token handling 7

--- client/src/api/index.tsx
+++ client/src/api/index.tsx
@@ -1,22 +1,15 @@
 import axios from "axios";
 
 const api = axios.create({
-
-  baseURL: import.meta.env.VITE_BACKEND_URL || "https://jobify-1-whmc.onrender.com/api",
-  withCredentials: true, // ضروري جداً لإرسال الكوكيز إذا كنت تستخدمها
+  baseURL: "https://jobify-1-whmc.onrender.com/api", // حط الرابط مباشرة
 });
 
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
   }
-);
+  return config;
+});
 
 export default api;
