10ea5d72b3a17969d4dfe003ee975b5aed6dd97a
unknown <benboudinamousa@gmail.com>
2026-03-25 01:48:22 -0700

    Update Services component with animations final 8

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,16 +1,14 @@
 import express, { Router } from "express";
-import { login, logout, profile, register } from "../controllers/authController.ts";
-import upload from "../utils/multer.ts";
-import protect from "../middleware/protect.ts";
+import { login, logout, profile, register } from "../controllers/authController.js";
+import upload from "../utils/multer.js";
+import protect from "../middleware/protect.js";
 
 //1) router olusturma
 const router: Router = express.Router();
 
-//2)yollari belirler
 router.route("/register").post(upload.single("photo"), register);
 router.route("/login").post(login);
 router.route("/logout").post(logout);
 router.route("/profile").get(protect, profile);
 
-//3)router'i export et
 export default router;
